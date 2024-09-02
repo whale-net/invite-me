a63f2cdfb1c4f1a5b1a26bbc1e5e25e645393f98
Connor Stabnick <cstabnick@gmail.com>
2024-09-02 00:50:28 -0400

    Notes for tomorrow

--- invite_me/service.py
+++ invite_me/service.py
@@ -6,6 +6,9 @@ class InvitationService:
     _request_response_uow: IRequestResponseUnitOfWork
 
     def __init__(self, request_response_uow: IRequestResponseUnitOfWork):
+        # todo: i want to be provided a dictionary of dispatchers
+        # a user could have a preferred dispatcher, so we will need some user data in the database
+        # could it be a user_dispatchers table with a priority rank assigned?
         self._request_response_uow = request_response_uow
 
     def create_request(self, request: Request):
