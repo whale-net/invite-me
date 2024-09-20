5134bc74cc9d199aac435d20d1e71703a0935431
Connor Stabnick <cstabnick@gmail.com>
2024-09-20 00:00:52 -0400

    fix path, add note

--- invite_me/__init__.py
+++ invite_me/__init__.py
@@ -5,6 +5,10 @@ from sqlmodel import SQLModel
 
 
 def seed_db():
+    """
+    Right now this runs on every restart of the application, which is frequent when we use hot reloading
+    todo: move to alembic for db migrations
+    """
     from invite_me.db import engine
     # import all table clases
     from .model import Request, Response, User  # noqa
